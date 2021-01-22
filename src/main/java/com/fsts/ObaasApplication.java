e9f3a6245997221e818deebd4f3bec0f1872147d
LENOVO <LENOVO@DESKTOP-LK3EEVG>
2021-01-22 12:16:44 +0100

    First commit

---
+++ src/main/java/com/fsts/ObaasApplication.java
@@ -0,0 +1,13 @@
+package com.fsts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObaasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObaasApplication.class, args);
+	}
+
+}
